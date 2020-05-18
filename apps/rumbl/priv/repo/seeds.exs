ba6fb4264ed92f34d18598fb4357cb2509f50f23
TsrAndrei <4539062+tsrandrei@users.noreply.github.com>
2020-05-18 17:44:01 +0200

    predeploy

--- apps/rumbl/priv/repo/seeds.exs
+++ apps/rumbl/priv/repo/seeds.exs
@@ -10,7 +10,65 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 alias Rumbl.Multimedia
+alias Rumbl.Accounts
+
 
 for category <- ~w(Action Drama Romance Comedy Sci-fi) do
   Multimedia.create_category!(category)
 end
+
+default_users = [
+	%{
+		name: "tsrandrei",
+		username: "tsrandrei",
+		password: "password",	
+	},
+	%{
+		name: "Jose V.",
+		username: "josev",
+		password: "josevpassword",	
+	},
+	%{
+		name: "Bruce T.",
+		username: "brucet",
+		password: "brucetpassword",	
+	},
+	%{
+		name: "Chris Mc",
+		username: "chrismc",
+		password: "chrismcpassword",	
+	},
+]
+
+default_videos = [
+	%{
+		title: "GOTO 2018 A Guide for the Perplexed Joe Armstrong",
+		url: "https://www.youtube.com/watch?v=rmueBVrLKcY",
+		description: "This presentation was recorded at GOTO Chicago 2018. #gotocon #gotochgo http://gotochgo.com"
+		},
+	%{
+		title: "GOTO 2018 A Guide for the Perplexed Joe Armstrong",
+		url: "https://www.youtube.com/watch?v=rmueBVrLKcY",
+		description: "This presentation was recorded at GOTO Chicago 2018. #gotocon #gotochgo http://gotochgo.com"
+		},
+	%{
+		title: "GOTO 2018 A Guide for the Perplexed Joe Armstrong",
+		url: "https://www.youtube.com/watch?v=rmueBVrLKcY",
+		description: "This presentation was recorded at GOTO Chicago 2018. #gotocon #gotochgo http://gotochgo.com"
+		},
+	%{
+		title: "GOTO 2018 A Guide for the Perplexed Joe Armstrong",
+		url: "https://www.youtube.com/watch?v=rmueBVrLKcY",
+		description: "This presentation was recorded at GOTO Chicago 2018. #gotocon #gotochgo http://gotochgo.com"
+		},
+	%{
+		title: "GOTO 2018 A Guide for the Perplexed Joe Armstrong",
+		url: "https://www.youtube.com/watch?v=rmueBVrLKcY",
+		description: "This presentation was recorded at GOTO Chicago 2018. #gotocon #gotochgo http://gotochgo.com"		
+		},
+]
+for user <- default_users do
+	Accounts.register_user(user)
+end
+
+Enum.map(default_videos, fn video -> Multimedia.create_video(Accounts.get_user_by(%{username: "tsrandrei"}), video) end)
